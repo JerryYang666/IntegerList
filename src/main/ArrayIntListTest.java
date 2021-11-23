c79315a8981bcf302361942a70810b09a5f4ec36
yangrh <ruihuangyang@hotmail.com>
2021-11-22 21:22:48 -0500

    add test file

---
+++ src/main/ArrayIntListTest.java
@@ -0,0 +1,58 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayIntListTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void indexOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ensureCapacity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void resize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testResize() {
+    }
+}
